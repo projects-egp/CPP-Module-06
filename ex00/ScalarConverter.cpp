deda663294540a08f168d16f643f10e275988ea0
Enrique Gil Piqueras <enrgil-p@c1r5s1.42madrid.com>
2026-08-25 21:54:43 +0200

    To be honest, no idea what I am doing

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/25 14:39:04 by enrgil-p          #+#    #+#             */
-/*   Updated: 2026/08/25 17:35:49 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2026/08/25 20:36:21 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,11 +33,23 @@ ScalarConverter::~ScalarConverter() {}
 //
 /*	*	*	CONVERTER && UTILS	*	*	*/
 
-static void	ScalarConverter::convert(const std::string& input)
+static bool	isChar(const std::string& input, t_types *types)
+{
+	if (input.length() == 1 && )
+	return true;
+}
+
+static void	typeDetect(const std::string& input, t_types *types)
+{
+	if (isChar(input, types))
+}
+
+void	ScalarConverter::convert(const std::string& input)
 {
 	t_types	types;
+	std::string::iterator	i;
 
-	//Detect type
+	//typeDetect(input, &types);
 	//Convert string to that type
 	//From converted type, convert to other types
 	//Print all
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,34 +6,18 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/19 15:09:07 by enrgil-p          #+#    #+#             */
-/*   Updated: 2026/08/25 17:45:11 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2026/08/25 20:35:59 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "general.hpp"
 
-static bool	storeInput(const std::string& input)
-{
-	/*char	chr;
-	int	integer;
-	float	flot;
-	double	dbl;
-*/
-	std::cout << input.length() << std::endl;
-	//Identify type
-	//store it in it's actual type
-	//convert to other types WITH CLASS "scalarConverter" !!!!!!!!
-	//print all
-	return true;
-}
-
 int	main(int c, char** v)
 {
 	if (c == 2)
 	{
-		//convert(v[1]);//from ScalarConverter. review cpp02
-		if (storeInput(v[1]))
-			return 0;
+		ScalarConverter::convert(v[1]);
+		return 0;
 	}
 	return 1;
 }
