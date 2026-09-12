6dea80305e3b2b75bbfd5dfb26faa0a8a4edc45c
Enrique Gil Piqueras <enrgil-p@c2r13s1.42madrid.com>
2026-09-12 19:27:52 +0200

    ex01, created Serializer.cpp. Seems to be easy, just call a cast. Not done this yet

---
+++ ex01/Serializer.cpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Serializer.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: enrgil-p <enrgil-p@student.42madrid.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/12 19:14:18 by enrgil-p          #+#    #+#             */
+/*   Updated: 2026/09/12 19:20:50 by enrgil-p         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "general.hpp"
+
+Serializer::Serializer() {}
+
+Serializer::Serializer(const Serializer& other) {}
+
+Serializer&	Serializer::operator=(const Serializer& rhs) {return *this;}
+
+Serializer::~Serializer() {}
